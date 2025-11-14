2e78a30a703ad0f90959a8a9110d37b056716381
Shreyash Jaiswal <shreyash5709@gmail.com>
2025-11-14 20:03:05 +0530

    redux toolkit project

---
+++ 11-reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Todos from './components/Todos.jsx'
+import AddTodo from './components/AddTodo.jsx'
+
+function App() {
+  return (
+    <>
+      <h1>Learn About Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
