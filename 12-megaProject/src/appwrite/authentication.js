64aa42b57b9a782337de39ddb6efe9755acff727
Shreyash Jaiswal <shreyash5709@gmail.com>
2025-11-19 21:01:08 +0530

    feat: initialize 12-megaProject with Vite and React setup
    - Add package.json with dependencies and scripts for development, build, linting, and preview.
    - Add AuthenticationService for handling user authentication with Appwrite.
    - Create AppwriteService for managing posts and file uploads with Appwrite.
    - Configure environment variables in config.js for Appwrite integration..

---
+++ 12-megaProject/src/appwrite/authentication.js
@@ -0,0 +1,65 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthenticationService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteEndpoint)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email, 
+                password, 
+                name
+            );
+
+            if (userAccount) {
+                // call another method to login the user after successful account creation
+                return this.login({email, password});
+            } else { 
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }   
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        }
+        catch (error) {
+            throw error;
+        }   
+    }
+
+    async grtCurrentUser() {
+        try {
+            return await this.account.get();
+        }
+        catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }   
+        return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error); 
+        }
+    }
+}
+
+const authenticationService = new AuthenticationService();
+export default authenticationService;
+
